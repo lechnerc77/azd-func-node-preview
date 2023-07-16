d2016e0e45260352b56b8d468bd8aaf78a5c4fa7
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-07-16 14:19:17 +0200

    feat: refinement of fucntion setup

--- src/api/functions/sendEmailFunction.ts
+++ src/api/functions/sendEmailFunction.ts
@@ -1,14 +1,14 @@
 const { app } = require('@azure/functions');
-const { json } = require('stream/consumers')
 
 app.http('sendEmail', {
     methods: ['POST'],
+    authLevel: 'function',
     handler: async (request, context) => {
         context.log(`HTTP triggered function processed request for url "${request.url}"`);
 
-        const data = await json(request.body);
-        context.log(data);
+        const requestBody = await request.json();
+        context.log(requestBody);
 
-        return { body: `Hello, world! Welcome to Azure Functions with the new programming model ${data.name}` };
+        return { body: `Hello, world! Welcome to Azure Functions with the new programming model ${requestBody.name}` };
     }
 });
